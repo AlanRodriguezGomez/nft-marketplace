eab8f5f2a45d0f5c0e54bf0cdb53b58a3b12ad34
AlanRodriguezGomez <alan.rodriguez.tcp@gmail.com>
2022-10-21 18:19:00 -0500

    Se agrega proyecto funcional marketplace

---
+++ webapp/src/projectsetting.ts
@@ -0,0 +1,3 @@
+export const addressContract = '0x5fbdb2315678afecb367f032d93f642f64180aa3'
+export const addressNFTContract = '0x5fbdb2315678afecb367f032d93f642f64180aa3'
+export const addressMarketContract = '0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0'
